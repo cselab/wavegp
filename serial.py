d3a8ef3980586d97d8da62c00af42d91ce9f78f4
Sergey Litvinov <slitvinov@gmail.com>
2024-12-10 15:09:32 +0100

    add serial.py

---
+++ serial.py
@@ -0,0 +1,72 @@
+import numpy as np
+import struct
+
+
+class UnknownFormatString(Exception):
+    pass
+
+
+dtype = np.dtype("uint8")
+
+
+def serial(fmt, *args):
+    assert len(fmt) == len(args)
+    buf = bytearray()
+    for f, a in zip(fmt, args):
+        if f == "i":
+            buf.extend(struct.pack("<i", a))
+        elif f == "S":
+            buf.extend(struct.pack("<i", len(a)))
+            for s in a:
+                buf.extend(struct.pack("<i", len(s)))
+                buf.extend(s.encode("utf-8"))
+        elif f == "y":
+            buf.extend(struct.pack("<i", len(a)))
+            for s in a:
+                assert s.dtype == dtype
+                buf.extend(struct.pack("<i", s.ndim))
+                buf.extend(struct.pack("<%ii" % s.ndim, *s.shape))
+                buf.extend(s.tobytes())
+        else:
+            raise UnknownFormatString("Unknown format string: %s" % f)
+    return buf
+
+
+def deserial(fmt, buf):
+    offset = 0
+    ans = []
+    for f in fmt:
+        if f == "i":
+            a, = struct.unpack_from("<i", buf, offset)
+            offset += struct.calcsize("<i")
+            ans.append(a)
+        elif f == "S":
+            n_strings, = struct.unpack_from("<i", buf, offset)
+            offset += struct.calcsize("<i")
+            strings = []
+            for i in range(n_strings):
+                size, = struct.unpack_from("<i", buf, offset)
+                offset += struct.calcsize("<i")
+                value, = struct.unpack_from("%ds" % size, buf, offset)
+                offset += struct.calcsize("%ds" % size)
+                strings.append(value.decode("utf-8"))
+            ans.append(strings)
+        elif f == "y":
+            n_arrays, = struct.unpack_from("<i", buf, offset)
+            offset += struct.calcsize("<i")
+            arrays = []
+            for i in range(n_arrays):
+                ndim, = struct.unpack_from("<i", buf, offset)
+                offset += struct.calcsize("<i")
+                shape = []
+                for i in range(ndim):
+                    d, = struct.unpack_from("<i", buf, offset)
+                    offset += struct.calcsize("<i")
+                    shape.append(d)
+                array = np.ndarray(shape, dtype, buf, offset)
+                arrays.append(array)
+                offset += array.size
+            ans.append(arrays)
+        else:
+            raise UnknownFormatString("Unknown format string: %s" % f)
+    return ans
--- write.py
+++ write.py
@@ -1,11 +1,37 @@
 import numpy as np
+import struct
+import serial
+from random import randrange
+
+
+def init():
+    gen = np.zeros((g.i + g.n + g.o, 1 + g.a + g.p), dtype=np.uint8)
+    for j in range(g.n):
+        gen[g.i + j, 0] = randrange(len(g.names))
+        for k in range(g.a):
+            gen[g.i + j, 1 + k] = randrange(g.i + j)
+        for k in range(g.p):
+            gen[g.i + j, 1 + g.a + k] = randrange(g.max_val)
+    for j in range(g.o):
+        gen[g.i + g.n + j, 1] = randrange(g.i + g.n)
+    return gen
+
+
 class g:
     pass
+
+
 g.names = "Backward_X", "Forward_X", "Backward_Y", "Forward_Y", "Plus"
 # input, maximum node, otuput, arity, parameters
 g.i = 1
 g.n = 10
 g.o = 1
 g.a = 2
-g.p = 0
-gene = np.zeros((g.i + g.n + g.o, 1 + g.a + g.p), dtype=np.uint8)
+g.p = 1
+g.max_val = 256
+g.lmb = 10
+genes = [init() for i in range(g.lmb)]
+fmt = "iiiiiSy"
+buf = serial.serial(fmt, g.i, g.n, g.o, g.a, g.p, g.names, genes)
+result = serial.deserial(fmt, buf)
+print(result)

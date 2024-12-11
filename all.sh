8923941258a856e150500b6f5042008920a97709
Sergey Litvinov <slitvinov@gmail.com>
2024-12-11 14:19:35 +0100

    gsize

--- all.sh
+++ all.sh
@@ -2,5 +2,5 @@
 python build.py a.raw
 python read.py a.raw
 python graphviz.py a.raw a
-for i in a.*.gv; do dot $i -T svg -o ${i%.gv}.svg; done
+for i in a.*.gv; do dot $i -Gsize=4,4\! -T svg -o ${i%.gv}.svg; done
 cp a.00000000.svg a.00000001.svg img/
